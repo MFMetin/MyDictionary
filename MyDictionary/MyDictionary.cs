dc426abe4101cf3d4e29da9a62e3cec7475b98a3
Furkan Metin <44furkan.metin@gmail.com>
2021-03-18 16:18:19 +0300

    sa

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] myKey;
+        Tvalue[] myValue;
+        Tkey[] temp1;
+        Tvalue[] temp2;
+        public MyDictionary()
+        {
+            myKey = new Tkey[0];
+            myValue = new Tvalue[0];        
+        }
+        public void Add(Tkey x, Tvalue y )
+        {
+            temp1 = myKey;
+            temp2 = myValue;
+
+            myKey = new Tkey[myKey.Length + 1];
+            myValue = new Tvalue[myValue.Length + 1];
+
+            for(int i = 0; i < temp1.Length; i++)
+            {
+                myKey[i] = temp1[i];
+                myValue[i] = temp2[i];            
+            }
+            myKey[myKey.Length - 1] = x;
+            myValue[myValue.Length - 1] = y;
+        }
+        public void List()
+        {
+            for (int i = 0; i < myKey.Length; i++)
+            {
+                Console.WriteLine("Key : " + myKey[i]);
+                Console.WriteLine("Value : " + myValue[i]);
+            }      
+        }
+        public void Find(Tkey find)
+        {
+            bool isFind = false;
+            
+            for (int i = 0; i < myKey.Length; i++)
+            {
+                if (find.Equals(myKey[i]))
+                {
+                    isFind = true;
+                    Console.WriteLine(myValue[i]);
+                }
+            }
+            if (isFind!)
+            {
+                Console.WriteLine("Key does not exist.");
+            }    
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary= new MyDictionary<int, string>();
+
+            myDictionary.Add(5, "Metin");
+            myDictionary.Add(6, "Sado");
+            myDictionary.Add(7, "Tufan");
+            myDictionary.Add(8, "Esref");
+            myDictionary.Add(9, "Rahman");
+
+
+            myDictionary.List();
+            myDictionary.Find(8);
+            myDictionary.Find(1);
+        }
+    }
+}
